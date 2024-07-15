469caf7932a7e4c418c88838cd1f1cd9321a299e
Ethan Quaresma <xorrdev@gmail.com>
2024-07-14 23:49:18 -0700

    Update plugins.sh

--- plugins.sh
+++ plugins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl "https://cdn.modrinth.com/data/fRQREgAc/versions/QtTWJjW6/Dynmap-3.7-beta-6-spigot.jar"
-curl "https://github.com/TownyAdvanced/Dynmap-Towny/releases/download/1.1.2/Dynmap-Towny-1.1.2.jar"
-curl "https://github.com/Imprex-Development/orebfuscator/releases/download/5.5.0/orebfuscator-plugin-5.5.0.jar"
-curl "https://c10.patreonusercontent.com/4/patreon-media/p/post/104200466/1ed5f46013c447bab8f5fe10f445def6/eyJhIjoxLCJwIjoxfQ%3D%3D/1?token-time=1721347200&token-hash=yDBRABEtVzkpCfTvgpM4Hg9VK8orSTlUKt3ZQSwrkCQ%3D" -O 'CoreProtect.jar'
-curl "https://www.spigotmc.org/resources/luckperms.28140/download?version=544174" -O 'LuckPerms-Bukkit-5.4.131.jar'
-curl "https://github.com/TownyAdvanced/Towny/releases/download/0.100.3.7/towny-0.100.3.7.jar" -O "towny-0.100.3.7.jar"
-curl "https://github.com/TownyAdvanced/TownyCultures/releases/download/2.0.2/TownyCultures-2.0.2.jar" -O 'TownyCultures-2.0.2.jar'
-curl "https://www.spigotmc.org/resources/veinminer.12038/download?version=532425" -O 'veinminer-bukkit-2.2.4.jar'
-curl "https://dl2.9minecraft.net/index.php?act=download&id=1718809010&hash=6694c0105a638" -O 'Spawn-Plugin-1.21.jar'
+curl "https://cdn.modrinth.com/data/fRQREgAc/versions/QtTWJjW6/Dynmap-3.7-beta-6-spigot.jar" --output 'Dynmap-3.7-beta-6-spigot.jar'
+curl "https://github.com/TownyAdvanced/Dynmap-Towny/releases/download/1.1.2/Dynmap-Towny-1.1.2.jar" --output 'Dynmap-Towny-1.1.2.ja'
+curl "https://github.com/Imprex-Development/orebfuscator/releases/download/5.5.0/orebfuscator-plugin-5.5.0.jar" --output 'orebfuscator-plugin-5.5.0.jar'
+curl "https://c10.patreonusercontent.com/4/patreon-media/p/post/104200466/1ed5f46013c447bab8f5fe10f445def6/eyJhIjoxLCJwIjoxfQ%3D%3D/1?token-time=1721347200&token-hash=yDBRABEtVzkpCfTvgpM4Hg9VK8orSTlUKt3ZQSwrkCQ%3D" --output 'CoreProtect.jar'
+curl "https://www.spigotmc.org/resources/luckperms.28140/download?version=544174" --output 'LuckPerms-Bukkit-5.4.131.jar'
+curl "https://github.com/TownyAdvanced/Towny/releases/download/0.100.3.7/towny-0.100.3.7.jar" --output "towny-0.100.3.7.jar"
+curl "https://github.com/TownyAdvanced/TownyCultures/releases/download/2.0.2/TownyCultures-2.0.2.jar" --output 'TownyCultures-2.0.2.jar'
+curl "https://www.spigotmc.org/resources/veinminer.12038/download?version=532425" --output 'veinminer-bukkit-2.2.4.jar'
+curl "https://dl2.9minecraft.net/index.php?act=download&id=1718809010&hash=6694c0105a638" --output 'Spawn-Plugin-1.21.jar'
